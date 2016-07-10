a3e0cb943f2a007a2f5f6921ae973ea460466af0
Dayana95 <dayanalara95@gmail.com>
2016-07-09 21:10:11 -0600

    Update whole website

--- js/calculo.js
+++ js/calculo.js
@@ -99,4 +99,157 @@ $(document).ready(function  (){
 
         });
 
+	  $('#combo3').change(function() {
+	 	var Copcion1 = $("#combo3").val();
+	 	    
+
+
+	 	switch(Copcion1){
+			
+
+	 		case 'person3':
+	 		$("#precio1").val("$90 por persona");
+				
+	 		break;
+
+	 		case 'person4':
+	 		$("#precio1").val("$67 por persona");
+				
+	 		break;
+
+	 		case 'person5':
+	 		$("#precio1").val("$60 por persona");
+				
+	 		break;
+	 		case 'person6':
+	 		$("#precio1").val("$52 por persona");
+				
+	 		break;
+	 		case 'person7':
+	 		$("#precio1").val("$46 por persona");
+				
+	 		break;
+	 		case 'person8':
+	 		$("#precio1").val("$41 por persona");
+				
+	 		break;
+	 		case 'person9':
+	 		$("#precio1").val("$38 por persona");
+				
+	 		break;
+	 		case 'person10':
+	 		$("#precio1").val("$35 por persona");
+				
+	 		break;
+
+	 	
+
+	 	}
+	 	
+
+
+        });
+
+	    $('#combo4').change(function() {
+	 	var Copcion1 = $("#combo4").val();
+	 	    
+
+
+	 	switch(Copcion1){
+			
+
+	 		case 'person3':
+	 		$("#precio1").val("$96 por persona");
+				
+	 		break;
+
+	 		case 'person4':
+	 		$("#precio1").val("$78 por persona");
+				
+	 		break;
+
+	 		case 'person5':
+	 		$("#precio1").val("$66 por persona");
+				
+	 		break;
+	 		case 'person6':
+	 		$("#precio1").val("$59 por persona");
+				
+	 		break;
+	 		case 'person7':
+	 		$("#precio1").val("$54 por persona");
+				
+	 		break;
+	 		case 'person8':
+	 		$("#precio1").val("$50 por persona");
+				
+	 		break;
+	 		case 'person9':
+	 		$("#precio1").val("$47 por persona");
+				
+	 		break;
+	 		case 'person10':
+	 		$("#precio1").val("$45 por persona");
+				
+	 		break;
+
+	 	
+
+	 	}
+	 	
+
+
+        });
+
+         $('#combo5').change(function() {
+	 	var Copcion1 = $("#combo5").val();
+	 	    
+
+
+	 	switch(Copcion1){
+			
+
+	 		case 'person3':
+	 		$("#precio1").val("$108 por persona");
+				
+	 		break;
+
+	 		case 'person4':
+	 		$("#precio1").val("$86 por persona");
+				
+	 		break;
+
+	 		case 'person5':
+	 		$("#precio1").val("$74 por persona");
+				
+	 		break;
+	 		case 'person6':
+	 		$("#precio1").val("$67 por persona");
+				
+	 		break;
+	 		case 'person7':
+	 		$("#precio1").val("$59 por persona");
+				
+	 		break;
+	 		case 'person8':
+	 		$("#precio1").val("$54 por persona");
+				
+	 		break;
+	 		case 'person9':
+	 		$("#precio1").val("$51 por persona");
+				
+	 		break;
+	 		case 'person10':
+	 		$("#precio1").val("$49 por persona");
+				
+	 		break;
+
+	 	
+
+	 	}
+	 	
+
+
+        });
+
 });
