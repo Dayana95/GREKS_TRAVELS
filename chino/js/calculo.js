09d90225a7253999f142f8e4dfcb1bfe282882f4
Dayana95 <dayanalara95@gmail.com>
2016-07-12 19:44:36 -0600

    Translations Done

---
+++ chino/js/calculo.js
@@ -0,0 +1,256 @@
+$(document).ready(function  (){
+	
+	 $('#combo1').change(function() {
+	 	var Copcion1 = $("#combo1").val();
+	 	    
+
+
+	 	switch(Copcion1){
+			case 'person4':
+	 		$("#precio1").val("$84 美元每人");
+				
+	 		break;
+
+	 		case 'person5':
+	 		$("#precio1").val("$67 美元每人");
+				
+	 		break;
+	 		case 'person6':
+	 		$("#precio1").val("$67 美元每人");
+				
+	 		break;
+	 		case 'person7':
+	 		$("#precio1").val("$67 美元每人");
+				
+	 		break;
+	 		case 'person8':
+	 		$("#precio1").val("$64 美元每人");
+				
+	 		break;
+	 		case 'person9':
+	 		$("#precio1").val("$50 美元每人");
+				
+	 		break;
+	 		case 'person10':
+	 		$("#precio1").val("$48 美元每人");
+				
+	 		break;
+
+	 	
+
+	 	}
+	 	
+
+
+        });
+
+
+	 $('#combo2').change(function() {
+	 	var Copcion1 = $("#combo2").val();
+	 	    
+
+
+	 	switch(Copcion1){
+			case 'person2':
+	 		$("#precio1").val("$96 美元每人");
+				
+	 		break;
+
+	 		case 'person3':
+	 		$("#precio1").val("$92 美元每人");
+				
+	 		break;
+
+	 		case 'person4':
+	 		$("#precio1").val("$75 美元每人");
+				
+	 		break;
+
+	 		case 'person5':
+	 		$("#precio1").val("$76 美元每人");
+				
+	 		break;
+	 		case 'person6':
+	 		$("#precio1").val("$74 美元每人");
+				
+	 		break;
+	 		case 'person7':
+	 		$("#precio1").val("$72 美元每人");
+				
+	 		break;
+	 		case 'person8':
+	 		$("#precio1").val("$70 美元每人");
+				
+	 		break;
+	 		case 'person9':
+	 		$("#precio1").val("$56 美元每人");
+				
+	 		break;
+	 		case 'person10':
+	 		$("#precio1").val("$55 美元每人");
+				
+	 		break;
+
+	 	
+
+	 	}
+	 	
+
+
+        });
+
+	  $('#combo3').change(function() {
+	 	var Copcion1 = $("#combo3").val();
+	 	    
+
+
+	 	switch(Copcion1){
+			
+
+	 		case 'person3':
+	 		$("#precio1").val("$90 美元每人");
+				
+	 		break;
+
+	 		case 'person4':
+	 		$("#precio1").val("$67 美元每人");
+				
+	 		break;
+
+	 		case 'person5':
+	 		$("#precio1").val("$60 美元每人");
+				
+	 		break;
+	 		case 'person6':
+	 		$("#precio1").val("$52 美元每人");
+				
+	 		break;
+	 		case 'person7':
+	 		$("#precio1").val("$46 美元每人");
+				
+	 		break;
+	 		case 'person8':
+	 		$("#precio1").val("$41 美元每人");
+				
+	 		break;
+	 		case 'person9':
+	 		$("#precio1").val("$38 美元每人");
+				
+	 		break;
+	 		case 'person10':
+	 		$("#precio1").val("$35 美元每人");
+				
+	 		break;
+
+	 	
+
+	 	}
+	 	
+
+
+        });
+
+	    $('#combo4').change(function() {
+	 	var Copcion1 = $("#combo4").val();
+	 	    
+
+
+	 	switch(Copcion1){
+			
+
+	 		case 'person3':
+	 		$("#precio1").val("$96 美元每人");
+				
+	 		break;
+
+	 		case 'person4':
+	 		$("#precio1").val("$78 美元每人");
+				
+	 		break;
+
+	 		case 'person5':
+	 		$("#precio1").val("$66 美元每人");
+				
+	 		break;
+	 		case 'person6':
+	 		$("#precio1").val("$59 美元每人");
+				
+	 		break;
+	 		case 'person7':
+	 		$("#precio1").val("$54 美元每人");
+				
+	 		break;
+	 		case 'person8':
+	 		$("#precio1").val("$50 美元每人");
+				
+	 		break;
+	 		case 'person9':
+	 		$("#precio1").val("$47 美元每人");
+				
+	 		break;
+	 		case 'person10':
+	 		$("#precio1").val("$45 美元每人");
+				
+	 		break;
+
+
+	 	
+
+	 	}
+	 	
+
+
+        });
+
+         $('#combo5').change(function() {
+	 	var Copcion1 = $("#combo5").val();
+	 	    
+
+
+	 	switch(Copcion1){
+			
+
+	 		case 'person3':
+	 		$("#precio1").val("$108 美元每人");
+				
+	 		break;
+
+	 		case 'person4':
+	 		$("#precio1").val("$86 美元每人");
+				
+	 		break;
+
+	 		case 'person5':
+	 		$("#precio1").val("$74 美元每人");
+				
+	 		break;
+	 		case 'person6':
+	 		$("#precio1").val("$67 美元每人");
+				
+	 		break;
+	 		case 'person7':
+	 		$("#precio1").val("$59 美元每人");
+				
+	 		break;
+	 		case 'person8':
+	 		$("#precio1").val("$54 美元每人");
+				
+	 		break;
+	 		case 'person9':
+	 		$("#precio1").val("$51 美元每人");
+				
+	 		break;
+	 		case 'person10':
+	 		$("#precio1").val("$49 美元每人");
+				
+	 		break;
+
+	 	
+
+	 	}
+	 	
+
+
+        });
+
+});
---
+++ ingles/js/calculo.js
@@ -0,0 +1,255 @@
+$(document).ready(function  (){
+	
+	 $('#combo1').change(function() {
+	 	var Copcion1 = $("#combo1").val();
+	 	    
+
+
+	 	switch(Copcion1){
+			case 'person4':
+	 		$("#precio1").val("$84 per person");
+				
+	 		break;
+
+	 		case 'person5':
+	 		$("#precio1").val("$67 per person");
+				
+	 		break;
+	 		case 'person6':
+	 		$("#precio1").val("$67 per person");
+				
+	 		break;
+	 		case 'person7':
+	 		$("#precio1").val("$67 per person");
+				
+	 		break;
+	 		case 'person8':
+	 		$("#precio1").val("$64 per person");
+				
+	 		break;
+	 		case 'person9':
+	 		$("#precio1").val("$50 per person");
+				
+	 		break;
+	 		case 'person10':
+	 		$("#precio1").val("$48 per person");
+				
+	 		break;
+
+	 	
+
+	 	}
+	 	
+
+
+        });
+
+
+	 $('#combo2').change(function() {
+	 	var Copcion1 = $("#combo2").val();
+	 	    
+
+
+	 	switch(Copcion1){
+			case 'person2':
+	 		$("#precio1").val("$96 per person");
+				
+	 		break;
+
+	 		case 'person3':
+	 		$("#precio1").val("$92 per person");
+				
+	 		break;
+
+	 		case 'person4':
+	 		$("#precio1").val("$75 per person");
+				
+	 		break;
+
+	 		case 'person5':
+	 		$("#precio1").val("$76 per person");
+				
+	 		break;
+	 		case 'person6':
+	 		$("#precio1").val("$74 per person");
+				
+	 		break;
+	 		case 'person7':
+	 		$("#precio1").val("$72 per person");
+				
+	 		break;
+	 		case 'person8':
+	 		$("#precio1").val("$70 per person");
+				
+	 		break;
+	 		case 'person9':
+	 		$("#precio1").val("$56 per person");
+				
+	 		break;
+	 		case 'person10':
+	 		$("#precio1").val("$55 per person");
+				
+	 		break;
+
+	 	
+
+	 	}
+	 	
+
+
+        });
+
+	  $('#combo3').change(function() {
+	 	var Copcion1 = $("#combo3").val();
+	 	    
+
+
+	 	switch(Copcion1){
+			
+
+	 		case 'person3':
+	 		$("#precio1").val("$90  per person");
+				
+	 		break;
+
+	 		case 'person4':
+	 		$("#precio1").val("$67  per person");
+				
+	 		break;
+
+	 		case 'person5':
+	 		$("#precio1").val("$60  per person");
+				
+	 		break;
+	 		case 'person6':
+	 		$("#precio1").val("$52  per person");
+				
+	 		break;
+	 		case 'person7':
+	 		$("#precio1").val("$46  per person");
+				
+	 		break;
+	 		case 'person8':
+	 		$("#precio1").val("$41  per person");
+				
+	 		break;
+	 		case 'person9':
+	 		$("#precio1").val("$38  per person");
+				
+	 		break;
+	 		case 'person10':
+	 		$("#precio1").val("$35  per person");
+				
+	 		break;
+
+	 	
+
+	 	}
+	 	
+
+
+        });
+
+	    $('#combo4').change(function() {
+	 	var Copcion1 = $("#combo4").val();
+	 	    
+
+
+	 	switch(Copcion1){
+			
+
+	 		case 'person3':
+	 		$("#precio1").val("$96 per person");
+				
+	 		break;
+
+	 		case 'person4':
+	 		$("#precio1").val("$78 per person");
+				
+	 		break;
+
+	 		case 'person5':
+	 		$("#precio1").val("$66 per person");
+				
+	 		break;
+	 		case 'person6':
+	 		$("#precio1").val("$59 per person");
+				
+	 		break;
+	 		case 'person7':
+	 		$("#precio1").val("$54 per person");
+				
+	 		break;
+	 		case 'person8':
+	 		$("#precio1").val("$50 per person");
+				
+	 		break;
+	 		case 'person9':
+	 		$("#precio1").val("$47 per person");
+				
+	 		break;
+	 		case 'person10':
+	 		$("#precio1").val("$45 per person");
+				
+	 		break;
+
+	 	
+
+	 	}
+	 	
+
+
+        });
+
+         $('#combo5').change(function() {
+	 	var Copcion1 = $("#combo5").val();
+	 	    
+
+
+	 	switch(Copcion1){
+			
+
+	 		case 'person3':
+	 		$("#precio1").val("$108 per person");
+				
+	 		break;
+
+	 		case 'person4':
+	 		$("#precio1").val("$86 per person");
+				
+	 		break;
+
+	 		case 'person5':
+	 		$("#precio1").val("$74 per person");
+				
+	 		break;
+	 		case 'person6':
+	 		$("#precio1").val("$67 per person");
+				
+	 		break;
+	 		case 'person7':
+	 		$("#precio1").val("$59 per person");
+				
+	 		break;
+	 		case 'person8':
+	 		$("#precio1").val("$54 per person");
+				
+	 		break;
+	 		case 'person9':
+	 		$("#precio1").val("$51 per person");
+				
+	 		break;
+	 		case 'person10':
+	 		$("#precio1").val("$49 per person");
+				
+	 		break;
+
+	 	
+
+	 	}
+	 	
+
+
+        });
+
+});
